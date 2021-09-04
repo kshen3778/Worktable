4a14cb99038c921c940c6fe99e0d48eec3cbbbaa
Kevin Shen <kshen3778@gmail.com>
2021-09-04 11:22:59 -0400

    restructure

--- workbench/modules/dataset.py
+++ workbench/modules/dataset.py
@@ -3,23 +3,23 @@ import json
 
 import pandas as pd
 from torch.utils.data import Dataset
+from torchvision.transforms import Compose
 
 class WorkbenchDataset(Dataset):
 
-    def __init__(self, base_dir,
-                 images,
+    def __init__(self,
+                 base_dir=None,
+                 images=None,
                  labels=None,
                  file_path_or_dataframe=None,
-                 get_item_as_dict=True,
-                 calculate_statistics=False,
-                 calculate_img_statistics=False):
+                 get_item_as_dict=True):
         """
         base_dir needs to be passed no matter what (all files underneath base_dir will be tracked for create_version)
         base_dir is also needed to store the .workbench file
         labels is optional if goal is inference
-        Option 1: images, labels are list of file paths containing base_dir
-        Option 2: images, labels are list of sub file paths from base_dir
-        Option 3: images, labels are column name of the dataframe/CSV specified by file_path_or_dataframe
+        Option 1: images, labels are list of relative file paths from the base_dir
+        Option 2: images, labels are column name of the dataframe/CSV specified by file_path_or_dataframe
+        (csv contain rel paths to base dir)
         If dataframe, then convert to csv when saving
 
         get_item_as_dict specifies whether __getitem__ will return (image, label) or {"image": x, "label": y}
@@ -32,21 +32,24 @@ class WorkbenchDataset(Dataset):
         }
         if labels is not None:
             self.profile["labels"] = labels
-        if isinstance(file_path_or_dataframe, pd.DataFrame):
-            self.profile["dataframe"] = file_path_or_dataframe
-        else:
-            self.profile["file_path"] = file_path_or_dataframe
-            # convert csv file to dataframe
-            self.profile["dataframe"] = pd.read_csv(file_path_or_dataframe)
-
-        if self.csv_path is not None:
-            self.profile["data_file_path"] = self.csv_path
 
         self.profile["get_item_as_dict"] = get_item_as_dict
 
-        # Option to calculate statistics
-        if calculate_statistics:
-            self.calculate_statistics(calculate_img_statistics)
+        # If loaded via file or dataframe
+        if file_path_or_dataframe is not None:
+            if isinstance(file_path_or_dataframe, pd.DataFrame):
+                # is a dataframe
+                self.profile["dataframe"] = file_path_or_dataframe
+            else:
+                # if a csv file
+                self.profile["file_path"] = file_path_or_dataframe
+                # convert csv file to dataframe
+                self.profile["dataframe"] = pd.read_csv(file_path_or_dataframe)
+
+            # extract paths
+            self.profile["images"] = self.profile["dataframe"][self.profile["images"]].to_numpy()
+            if labels is not None:
+                self.profile["labels"] = self.profile["dataframe"][self.profile["labels"]].to_numpy()
 
 
     def attach_transformations(self, transforms, train=True):
@@ -59,6 +62,21 @@ class WorkbenchDataset(Dataset):
             self.profile["transforms"]["train"] = transforms
         else:
             self.profile["transforms"]["inference"] = transforms
+
+    def get_transformations(self, compose=False, train=True):
+        """
+        Return transformations as list of a Compose
+        """
+        transform_list = []
+        if train:
+            transform_list = self.profile["transforms"]["train"]
+        else:
+            transform_list = self.profile["transforms"]["inference"]
+
+        if compose:
+            return Compose(transform_list)
+        return transform_list
+
 
     def remove_transformations(self, train=True):
         """
@@ -103,10 +121,11 @@ class WorkbenchDataset(Dataset):
         """
         return self.profile
 
-    def calculate_statistics(self, calculate_img_stats=False):
+    def calculate_statistics(self, percentiles=[]):
         """
-        Overall: min, max, std, percentiles, spacing, number of classes, number of pixels/voxels per class
-        Individual: same as above but for each individual image (if calculate_img_stats = True)
+        Min, max, mean, std, percentiles, spacing, number of classes, number of pixels/voxels per class
+        Calculate first 6 for individual images as well
+        percentiles will specify the xth percentile when calculating percentiles
         """
         return NotImplementedError
 
@@ -128,9 +147,9 @@ class WorkbenchDataset(Dataset):
 
 
     def create_new_version(self,
-                           preprocessing=None,
                            new_base_dir=None,
-                           save_profile=False):
+                           preprocessing=None,
+                           save_profile=True):
         """
         Create a new dataset version on disk (that can be loaded through WorkbenchDataset)
 
@@ -138,7 +157,7 @@ class WorkbenchDataset(Dataset):
 
         Create a new copy at a new location and apply all preprocessing
 
-        Optionally save the profile as a workbench file (in the new base dir)
+        Save the profile as a workbench file (in the new base dir)
         """
         return NotImplementedError
 
@@ -149,6 +168,11 @@ class WorkbenchDataset(Dataset):
         Items is a list of indices or list of file paths
         """
         return NotImplementedError
+
+    def set_label_names(self, names=[]):
+        """
+        Set string names for integer class labels
+        """
 
     def __getitem__(self, item):
         return NotImplementedError
--- workbench/modules/nifti_dataset.py
+++ workbench/modules/nifti_dataset.py
@@ -1,4 +1,37 @@
-from workbench.modules.dataset import Dataset
+from workbench.modules.dataset import WorkbenchDataset
 
-class NIFTIDataset(Dataset):
-    pass
+class NIFTIDataset(WorkbenchDataset):
+
+    def __init__(self,
+                 base_dir=None,
+                 images=None,
+                 labels=None,
+                 file_path_or_dataframe=None,
+                 get_item_as_dict=True,
+                 calculate_statistics=False):
+        """
+        calculate_statistics decides whether to run calculate_statistics() on init
+        """
+        super().__init__(base_dir, images, labels, file_path_or_dataframe, get_item_as_dict)
+
+        # Option to calculate statistics directly as init
+        if calculate_statistics:
+            self.calculate_statistics()
+
+
+    def calculate_statistics(self, percentiles=[]):
+        # Max/min
+
+        # Mean
+
+        # Std
+
+        # Percentiles
+
+        # Image spacing
+
+        # Number of classes
+
+        # Number of pixels/voxels per class
+
+        pass
